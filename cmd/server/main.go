d936bc3bb45f41af9ee24ffb3f2d6d9d487694f1
ctcsar <ctcsar12@gmail.com>
2024-09-27 19:28:22 +0300

    first try

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,57 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+	"strconv"
+)
+
+type MemStorage struct {
+	gauge   float64
+	counter []int64
+}
+
+func (g *MemStorage) setStorage(v, t string) {
+	switch t {
+	case "gauge":
+		val, err := strconv.ParseFloat(v, 64)
+		if err != nil {
+			panic(err)
+		}
+		g.gauge = val
+	case "counter":
+		val, err := strconv.ParseInt(v, 10, 64)
+		if err != nil {
+			panic(err)
+		}
+		g.counter = append(g.counter, val)
+	}
+}
+func webhook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	storrage := &MemStorage{}
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+	if r.PathValue("type") != "gauge" && r.PathValue("type") != "counter" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	if r.PathValue("name") == " " {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	storrage.setStorage(r.PathValue("value"), r.PathValue("type"))
+	w.WriteHeader(http.StatusOK)
+}
+
+func run() error {
+	return http.ListenAndServe(`:8080`, nil)
+}
+
+func main() {
+	http.HandleFunc("/update/{type}/{name}/{value}", webhook)
+	if err := run(); err != nil {
+		panic(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ctcsar/metric-and-alerting-system-yp
+
+go 1.22.2
