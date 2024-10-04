0fcd836c991bb3e276c1a6814522c225c1cb6da7
ctcsar <ctcsar12@gmail.com>
2024-10-04 17:58:35 +0300

    first try

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,34 +1,40 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"strconv"
 )
 
-type MemStorage struct {
-	gauge   float64
-	counter []int64
+type Storage struct {
+	gauge   map[string]float64
+	counter int64
 }
 
-func (g *MemStorage) setStorage(v, t string) {
+func (g *Storage) String() string {
+	return fmt.Sprintf("Storage{gauge: %+v, counter: %d}", g.gauge, g.counter)
+}
+
+func (g *Storage) setStorage(v, t, n string) {
 	switch t {
 	case "gauge":
 		val, err := strconv.ParseFloat(v, 64)
 		if err != nil {
 			panic(err)
 		}
-		g.gauge = val
+		g.gauge = map[string]float64{n: val}
 	case "counter":
 		val, err := strconv.ParseInt(v, 10, 64)
 		if err != nil {
 			panic(err)
 		}
-		g.counter = append(g.counter, val)
+		g.counter = val
 	}
 }
 func webhook(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	storrage := &MemStorage{}
+
+	m := Storage{}
 	if r.Method != http.MethodPost {
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		return
@@ -42,8 +48,13 @@ func webhook(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	storrage.setStorage(r.PathValue("value"), r.PathValue("type"))
+	m.setStorage(r.PathValue("value"), r.PathValue("type"), r.PathValue("name"))
 	w.WriteHeader(http.StatusOK)
+
+	for k, v := range m.gauge {
+		fmt.Fprintf(w, "%s: %f\n", k, v)
+	}
+	fmt.Fprintf(w, "counter: %d\n", m.counter)
 }
 
 func run() error {
