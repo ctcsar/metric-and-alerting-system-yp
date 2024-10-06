974985b47774c7ccc02c05b3764c9266e3e2706a
ctc <ctc@ctcs-MacBook-Air.local>
2024-10-07 00:35:42 +0300

    try 2

--- internal/storage/server_storage.go
+++ internal/storage/server_storage.go
@@ -25,8 +25,8 @@ func GetStorageInstance() *Storage {
 	return storageInstance
 }
 
-func (g *Storage) String() string {
-	return fmt.Sprintf("Storage{gauge: %+v, counter: %d}", g.Gauge, g.Counter)
+func (m *Storage) String() string {
+	return fmt.Sprintf("Storage{gauge: %+v, counter: %d}", m.Gauge, m.Counter)
 }
 func (m *Storage) GetMetricValue(metricType string, metricName string) (float64, bool) {
 	if metricType == "gauge" {
@@ -44,19 +44,19 @@ func (m *Storage) GetAllMetrics() map[string]map[string]float64 {
 	metrics["counter"] = map[string]float64{"counter": float64(m.Counter)}
 	return metrics
 }
-func (g *Storage) SetStorage(v, t, n string) {
+func (m *Storage) SetStorage(v, t, n string) {
 	switch t {
 	case "gauge":
 		val, err := strconv.ParseFloat(v, 64)
 		if err != nil {
 			panic(err)
 		}
-		g.Gauge[n] = val
+		m.Gauge = map[string]float64{n: val}
 	case "counter":
 		val, err := strconv.ParseInt(v, 10, 64)
 		if err != nil {
 			panic(err)
 		}
-		g.Counter = val
+		m.Counter = val
 	}
 }
