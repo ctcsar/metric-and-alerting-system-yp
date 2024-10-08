df212ce87570a882a04943b8ce030ee2cc87c4df
ctc <ctc@ctcs-MacBook-Air.local>
2024-10-09 01:14:58 +0300

    try1

--- cmd/agent/flags.go
+++ cmd/agent/flags.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"flag"
+	"os"
+	"strconv"
 )
 
 var URLSend string
@@ -12,6 +14,16 @@ func parseFlags() {
 	flag.StringVar(&URLSend, "a", "localhost:8080", "address and port to run server")
 	flag.IntVar(&SendTime, "r", 10, "time in seconds to send metrics")
 	flag.IntVar(&GetTime, "p", 2, "time in seconds to get metrics")
-	// парсим переданные серверу аргументы в зарегистрированные переменные
 	flag.Parse()
+
+	if envRunAddr := os.Getenv("ADDRESS"); envRunAddr != "" {
+		URLSend = envRunAddr
+	}
+	if envSetTime := os.Getenv("REPORT_INTERVAL"); envSetTime != "" {
+		GetTime, _ = strconv.Atoi(envSetTime)
+	}
+	if envGetTime := os.Getenv("POLL_INTERVAL"); envGetTime != "" {
+		SendTime, _ = strconv.Atoi(envGetTime)
+	}
+
 }
--- cmd/server/flags.go
+++ cmd/server/flags.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"os"
 )
 
 // неэкспортированная переменная flagRunAddr содержит адрес и порт для запуска сервера
@@ -10,9 +11,14 @@ var flagRunAddr string
 // parseFlags обрабатывает аргументы командной строки
 // и сохраняет их значения в соответствующих переменных
 func ParseFlags() {
-	// регистрируем переменную flagRunAddr
-	// как аргумент -a со значением :8080 по умолчанию
 	flag.StringVar(&flagRunAddr, "a", "localhost:8080", "address and port to run server")
-	// парсим переданные серверу аргументы в зарегистрированные переменные
 	flag.Parse()
+
+	// для случаев, когда в переменной окружения RUN_ADDR присутствует непустое значение,
+	// переопределим адрес запуска сервера,
+	// даже если он был передан через аргумент командной строки
+	if envRunAddr := os.Getenv("RUN_ADDR"); envRunAddr != "" {
+		flagRunAddr = envRunAddr
+	}
+
 }
