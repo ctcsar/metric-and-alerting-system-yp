13350480245e84b7d50cf021faa236efaec17d76
ctc <ctc@ctcs-MacBook-Air.local>
2024-10-08 20:48:27 +0300

    try5

--- cmd/agent/flags.go
+++ cmd/agent/flags.go
@@ -9,7 +9,7 @@ var GetTime int
 var SendTime int
 
 func parseFlags() {
-	flag.StringVar(&URLSend, "a", "localhost:8080", "address and port to run server")
+	flag.StringVar(&URLSend, "a", "http://localhost:8080", "address and port to run server")
 	flag.IntVar(&GetTime, "p", 2, "time in seconds to get metrics")
 	flag.IntVar(&SendTime, "r", 10, "time in seconds to send metrics")
 	// парсим переданные серверу аргументы в зарегистрированные переменные
