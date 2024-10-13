651b1987d65d9e3e04b807091ae6b84d1b23d86d
ctc <ctc@ctcs-MacBook-Air.local>
2024-10-13 14:25:35 +0300

    fix: change constant names and fix codecov tests run

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -10,8 +10,8 @@ import (
 	"github.com/ctcsar/metric-and-alerting-system-yp/internal/storage"
 )
 
-const GAUGE_METRICS_TYPE = "gauge"
-const COUNTER_METRICS_TYPE = "counter"
+const GaugeMetricsType = "gauge"
+const CounterMetricsType = "counter"
 
 func main() {
 	handlers.GracefulShutdown()
@@ -24,13 +24,13 @@ func main() {
 		metrics := memStorage.Metrics
 		for k, v := range metrics.Gauge {
 
-			err := handlers.SendMetric(flags.GetURLForSend(), GAUGE_METRICS_TYPE, k, fmt.Sprintf("%f", v))
+			err := handlers.SendMetric(flags.GetURLForSend(), GaugeMetricsType, k, fmt.Sprintf("%f", v))
 			if err != nil {
 				fmt.Println(err)
 			}
 		}
 		for k, v := range metrics.Counter {
-			err := handlers.SendMetric(flags.GetURLForSend(), COUNTER_METRICS_TYPE, k, fmt.Sprintf("%d", v))
+			err := handlers.SendMetric(flags.GetURLForSend(), CounterMetricsType, k, fmt.Sprintf("%d", v))
 			if err != nil {
 				fmt.Println(err)
 			}
