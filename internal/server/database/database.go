c7f29c963d5f22f0296702d82ad272fffdecd851
ctc <ctc@ctcs-MacBook-Air.local>
2024-12-17 23:06:04 +0300

    fix: check migrations didnt breake tests

--- internal/server/database/database.go
+++ internal/server/database/database.go
@@ -3,58 +3,52 @@ package server
 import (
 	"context"
 	"database/sql"
-	"errors"
 	"fmt"
 	"log"
-	"time"
-
-	"github.com/jackc/pgerrcode"
 
 	"github.com/ctcsar/metric-and-alerting-system-yp/internal/server/storage"
-	"github.com/jackc/pgx"
 )
 
-const (
-	maxRetries = 3
-)
+// const (
+// 	maxRetries = 3
+// )
 
-var retryDelays = []time.Duration{
-	1 * time.Second,
-	3 * time.Second,
-	5 * time.Second,
-}
+// var retryDelays = []time.Duration{
+// 	1 * time.Second,
+// 	3 * time.Second,
+// 	5 * time.Second,
+// }
 
-func isRetriableError(err error) bool {
-	var pgErr *pgx.PgError
-	if errors.As(err, &pgErr) {
-		if pgErr.Code == pgerrcode.ConnectionDoesNotExist {
-			return true
-		}
-		if pgErr.Code == pgerrcode.ConnectionException {
-			return true
-		}
-	}
-	return false
-}
+// func isRetriableError(err error) bool {
+// 	var pgErr *pgx.PgError
+// 	if errors.As(err, &pgErr) {
+// 		if pgErr.Code == pgerrcode.ConnectionDoesNotExist {
+// 			return true
+// 		}
+// 		if pgErr.Code == pgerrcode.ConnectionException {
+// 			return true
+// 		}
+// 	}
+// 	return false
+// }
 
-func retryQuery(ctx context.Context, query func() error) error {
-	for i := 0; i < maxRetries; i++ {
-		select {
-		case <-ctx.Done():
-			return ctx.Err()
-		default:
-			err := query()
-			if err == nil {
-				return nil
-			}
-			if !isRetriableError(err) {
-				return err
-			}
-			time.Sleep(retryDelays[i])
-		}
-	}
-	return errors.New("failed after max retries")
-}
+// func retryQuery(ctx context.Context, query func() error) error {
+// 	for i := 0; i < maxRetries; i++ {
+// 		select {
+// 		case <-ctx.Done():
+// 		default:
+// 			err := query()
+// 			if err == nil {
+// 				return nil
+// 			}
+// 			if !isRetriableError(err) {
+// 				return err
+// 			}
+// 			time.Sleep(retryDelays[i])
+// 		}
+// 	}
+// 	return errors.New("failed after max retries")
+// }
 
 func DBConnect(dsn string) (*sql.DB, error) {
 	db, err := sql.Open("pgx", dsn)
@@ -82,20 +76,14 @@ func DBSaveMetrics(ctx context.Context, db *sql.DB, metrics *storage.Storage) er
 		}
 	}()
 	for k, v := range metrics.Gauge {
-		err = retryQuery(ctx, func() error {
-			_, err = tx.Exec("INSERT INTO gauge_metrics VALUES ($1, $2) ON CONFLICT (name) DO UPDATE SET value = $2", k, v)
-			return err
-		})
+		_, err = tx.Exec("INSERT INTO gauge_metrics VALUES ($1, $2) ON CONFLICT (name) DO UPDATE SET value = $2", k, v)
 		if err != nil {
 			return fmt.Errorf("error inserting gauge metric: %w", err)
 		}
 	}
 
 	for k, v := range metrics.Counter {
-		err = retryQuery(ctx, func() error {
-			_, err := tx.Exec("INSERT INTO counter_metrics VALUES ($1, $2) ON CONFLICT (name) DO UPDATE SET value = $2", k, v)
-			return err
-		})
+		_, err := tx.Exec("INSERT INTO counter_metrics VALUES ($1, $2) ON CONFLICT (name) DO UPDATE SET value = $2", k, v)
 		if err != nil {
 			return fmt.Errorf("error inserting counter metric: %w", err)
 		}
