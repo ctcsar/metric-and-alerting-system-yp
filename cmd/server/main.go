6cd00c49dbbd0f083f5d59acd0801811817d4ad2
ctc <ctc@ctcs-MacBook-Air.local>
2024-12-17 22:04:40 +0300

    fix: check migrations didnt breake tests

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -27,6 +27,7 @@ func main() {
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt)
 	ctx := context.Background()
+	defer ctx.Done()
 
 	metrics := storage.NewStorage()
 	handler := chi.NewRouter()
